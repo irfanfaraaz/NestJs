9925993d4ff1e51befe810b88a3cab399ede5ff4
irfan <irfanfaraaz03@gmail.com>
2023-04-02 23:29:46 +0530

    made changes

--- nestjs-api/dist/prisma/prisma.service.js
+++ nestjs-api/dist/prisma/prisma.service.js
@@ -11,9 +11,10 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.PrismaService = void 0;
 const common_1 = require("@nestjs/common");
+const config_1 = require("@nestjs/config");
 const client_1 = require("@prisma/client");
 let PrismaService = class PrismaService extends client_1.PrismaClient {
-    constructor() {
+    constructor(config) {
         super({
             datasources: {
                 db: {
@@ -25,7 +26,7 @@ let PrismaService = class PrismaService extends client_1.PrismaClient {
 };
 PrismaService = __decorate([
     (0, common_1.Injectable)(),
-    __metadata("design:paramtypes", [])
+    __metadata("design:paramtypes", [config_1.ConfigService])
 ], PrismaService);
 exports.PrismaService = PrismaService;
 //# sourceMappingURL=prisma.service.js.map
--- nestjs-api/src/prisma/prisma.service.ts
+++ nestjs-api/src/prisma/prisma.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(config: ConfigService) {
     super({
       datasources: {
         db: {
